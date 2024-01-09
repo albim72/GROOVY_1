3694f9b2fb8b8425dd95a60dd02d7c209dc5609c
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 12:12:26 +0100

    Create Marks.groovy

---
+++ DZIEN_2/ocena_studenta/Marks.groovy
@@ -0,0 +1,9 @@
+trait Marks implements Total {
+    void DisplayMarks(){
+        println("Wyświetlenie ocen...")
+    }
+    void DisplayTotal(){
+        println("Wyświetlenie sumy punktów za oceny...")
+    }
+
+}
