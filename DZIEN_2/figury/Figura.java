adb6bb8b4fc85e2ddb5536d43c2afd75281b8577
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 10:29:23 +0100

    Update Figura.java

--- DZIEN_2/figury/Figura.java
+++ DZIEN_2/figury/Figura.java
@@ -1,13 +1,25 @@
 public abstract class Figura {
     protected double a;
     protected double b;
+    protected int factor;
 
     public Figura(double a, double b) {
         this.a = a;
         this.b = b;
         this.info();
     }
-    
+
+    public Figura(double a) {
+        this.a = a;
+        this.info();
+    }
+
+
+    public Figura(double a, int factor) {
+        this.a = a;
+        this.factor = factor;
+    }
+
     public void info(){
         System.out.println("stworzono nowy obiekt oparty na klasie abstrakcyjnej Figura...");
     }
