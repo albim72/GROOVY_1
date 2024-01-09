327e07e4b1ce7ee181dcecf00dea8af66d619170
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 10:04:39 +0100

    Update Main.java

--- DZIEN_2/figury/Main.java
+++ DZIEN_2/figury/Main.java
@@ -5,5 +5,9 @@ public class Main {
         String nazwa =  pr.getClass().getSimpleName();
         System.out.printf("Figura: %s, pole: %f cm2\n",nazwa,pr.policzPole());
 
+        Trojkat tr = new Trojkat(6.6,8.9);
+        nazwa =  tr.getClass().getSimpleName();
+        System.out.printf("Figura: %s, pole: %f cm2\n",nazwa,tr.policzPole());
+
     }
 }
