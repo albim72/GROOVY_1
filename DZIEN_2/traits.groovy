2070eac963574a7d5756bb1eb3916f2cfe73a2ad
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 12:07:23 +0100

    Update traits.groovy

--- DZIEN_2/traits.groovy
+++ DZIEN_2/traits.groovy
@@ -1,13 +1,41 @@
+class Testowa{
+    int d;
 
-trait Informacje{
+    Testowa(int d) {
+        this.d = d
+    }
+
+    int showd(){
+        return d
+    }
+}
+
+trait gg {
+    String msg(){
+        "ważna informacja: gg"
+    }
+}
+interface abc{
+    double rabat(double a,double b)
+}
+
+trait Test implements abc{
+    int num = 100
+    int printNum(){
+        return num*2
+    }
+}
+
+trait Informacje implements Test{
   String pobierzInfo() {
     "to jest trait Informacje"
   }
+    abstract int oblicz(d)
 }
 
 trait Osoba{
     String imie //właściwość w trait
-    
+
         void UstawImie(String noweimie){
         imie = noweimie
     }
@@ -16,8 +44,28 @@ trait Osoba{
     }
 }
 
-class MojaKlasa implements Informacje, Osoba{
+interface dodatek{
+    boolean czyjest()
+}
 
+class MojaKlasa implements Informacje, Osoba, dodatek{
+
+
+    @Override
+    double rabat(double a, double b) {
+        return (a+b)*0.06
+    }
+
+    @Override
+    boolean czyjest() {
+        return true
+    }
+
+
+    @Override
+    int oblicz(java.lang.Object d) {
+        return d*9
+    }
 }
 
 static void main(String[] args) {
@@ -25,4 +73,8 @@ static void main(String[] args) {
     println(obiekt.pobierzInfo())
     obiekt.UstawImie("Karol")
     println(obiekt.pobierzImie())
+    println(obiekt.printNum())
+    println(obiekt.rabat(34,7))
+    println(obiekt.czyjest())
+    println(obiekt.oblicz(34))
 }
