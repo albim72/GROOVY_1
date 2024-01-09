e75e7d30a31626c293dfe7cd732f7be1d3f88b29
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 09:37:04 +0100

    Create prototyp.java

---
+++ DZIEN_2/abstrakcja/prototyp.java
@@ -0,0 +1,13 @@
+public abstract class Prototyp {
+    protected int x;
+
+    public Prototyp(int x) {
+        this.x = x;
+    }
+
+    public void info(){
+        System.out.println("to jest metoda nieabstrakcyjna klasy abstrakcyjnej");
+    }
+    public abstract double policz();
+    public abstract double policzX();
+}
