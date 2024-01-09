72ef4d16bb4c2fead566edf81d87c8ce2839c5c9
albim72 <marcin.albiniak72@gmail.com>
2024-01-09 15:58:12 +0100

    Update Sortbyname.java

--- DZIEN_2/komparator/Sortbyname.java
+++ DZIEN_2/komparator/Sortbyname.java
@@ -2,7 +2,10 @@ import java.util.Comparator;
 
 public class SortbyName implements Comparator<Student>{
     @Override
-    public String toString() {
-        return this.rollno + " " + this.name + " " + this.address;
+//    public String toString() {
+//        return this.rollno + " " + this.name + " " + this.address;
+//    }
+    public int compare(Student o1, Student o2) {
+        return o1.name.compareTo(o2.name);
     }
 }
