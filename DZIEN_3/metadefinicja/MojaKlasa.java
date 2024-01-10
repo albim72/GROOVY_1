4986e3cc64255e24efccb5c1fd21a2ea9e22554b
albim72 <marcin.albiniak72@gmail.com>
2024-01-10 15:12:27 +0100

    Create MojaKlasa.java

---
+++ DZIEN_3/metadefinicja/MojaKlasa.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class MojaKlasa {
+    private int number;
+    private String text;
+    
+    public void Info(){
+        System.out.println("to jest klasa testowa...");
+    }
+}
