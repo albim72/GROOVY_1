132fcc30cd99587ce10265d2476f845e72a0721b
albim72 <marcin.albiniak72@gmail.com>
2024-01-10 09:42:32 +0100

    Create GStringBuffer.groovy

---
+++ DZIEN_3/GStringBuffer.groovy
@@ -0,0 +1,12 @@
+static void main(String[] args) {
+  def sbuffer = new StringBuffer("Przykład 1: ")
+
+  sbuffer.append("To ")
+  sbuffer.append("jest ")
+  sbuffer.append("przykład  ")
+  sbuffer.append("użycia ")
+  sbuffer.append("StringBuffer ")
+  sbuffer.append("w Groovy! ")
+
+  println(sbuffer)
+}
